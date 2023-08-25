386e29366a1e26d27e5b6ca73b0aeaed37d12b24
lorinc-toldi <lorinc.toldi@gmail.com>
2023-08-25 19:59:50 +0200

    user agent

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -78,6 +78,9 @@ export default function Home() {
                 startYear: selectedMin?.code,
                 endYear: selectedMax?.code,
               },
+              headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+              }
             })
               .then((res) => {
                 setCars(res.data);
